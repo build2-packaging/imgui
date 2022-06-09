5dc9c02cfc97a1281be4cdc93a185b61b71f30ef
Fabian Meyer <rookfighter@posteo.org>
2022-06-09 20:17:16 +0200

    Use IMGUI_IMPL_API for backends

--- imgui/imgui/imconfig.h
+++ imgui/imgui/imconfig.h
@@ -25,15 +25,19 @@
 // for each static/DLL boundary you are calling from. Read "Context and Memory Allocators" section of imgui.cpp for more details.
 #ifdef _WIN32
 #ifndef IMGUI_STATIC
+
 #ifdef IMGUI_EXPORT
-
 #define IMGUI_API __declspec( dllexport )
-
 #else
-
 #define IMGUI_API __declspec( dllimport )
-
 #endif
+
+#ifdef IMGUI_IMPL_EXPORT
+#define IMGUI_IMPL_API __declspec( dllexport )
+#else
+#define IMGUI_IMPL_API __declspec( dllimport )
+#endif
+
 #endif
 #endif
 
